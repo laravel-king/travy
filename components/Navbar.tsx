082ef91d8e08325f8ddacd34ee6e31c8177a6c5c
laravel-king <laravelking@gmail.com>
2023-12-10 00:24:38 +0100

    Starting the navbar magic

---
+++ components/Navbar.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+const Navbar = () => {
+return (
+    <nav className="border-2 border-red-500 flexBetween max-container padding-container relative z-30 py-5">
+        <Link href="/">Index</Link>
+    </nav>
+)
+}
+export default Navbar;
