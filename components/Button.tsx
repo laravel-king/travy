d8f558c66c42a6eacb48cec7a418663aece98800
laravel-king <laravelking@gmail.com>
2023-12-10 19:01:24 +0100

    Adding call to action

--- components/Button.tsx
+++ components/Button.tsx
@@ -4,10 +4,11 @@ type ButtonProps = {
     type: 'button' | 'submit',
     title: string,
     icon?: string,
-    variant: string
+    variant: string,
+    full?: boolean,
 }
-const Button = ({type,title,icon, variant} : ButtonProps) => {
-return <button className={`flexCenter gap-3 rounded border ${variant}`} type={type} >
+const Button = ({type, title, icon, variant, full} : ButtonProps) => {
+return <button className={`flexCenter gap-3 rounded border ${variant} ${full && 'w-full'}`} type={type} >
     {icon && <Image src={icon} alt={title} width={20} height={20} /> }
     <label className="bold-16 whitespace-nowrap">{title}</label>
 </button>
